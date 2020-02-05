d968a553ba3a7797f46aaaf1aa083b5754d7d742
David Hoffman <david.r.hoffman@ieee.org>
2020-02-05 14:30:20 -0800

    add enlighter functionality

---
+++ config_md2wp_enlighter.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+# NOTE: in Sublime Text, "(groups) in regex" are "$1 in regex".
+#             in Python, "(groups) in regex" are "\g<1> in regex".
+
+
+# regular expression strings in format [(find, replace), (find2, replace2)...]
+regexStrings = [
+
+	(r" *\- ",
+	 r"""\n"""),
+
+	(r"""(.*)Codeblock \#(\d*): Lines (\d*)-(\d*)(.*)""",
+	 r"""\g<1>\n\n<!-- wp:enlighter/codeblock {"linenumbers":"true","group":@GROUPA1@,"language":"python","lineoffset":"\g<3>","title":"@TITLE@"} -->\n<pre class="EnlighterJSRAW" data-enlighter-language="python" data-enlighter-theme="" data-enlighter-highlight="" data-enlighter-linenumbers="true" data-enlighter-lineoffset="\g<3>" data-enlighter-title="@TITLE@" data-enlighter-group="@GROUPA2@">\nCodeblock \g<2> Lines \g<3>-\g<4></pre>\n<!-- /wp:enlighter/codeblock -->\n\n\g<5>"""),
+
+	(r"""(.*)COMMAND OUTPUT(.*)""",
+	 r"""\g<1>\n\n<!-- wp:enlighter/codeblock {"linenumbers":"true","group":@GROUPB1@,"language":"shell","lineoffset":"1","title":"@TITLE@"} -->\n<pre class="EnlighterJSRAW" data-enlighter-language="shell" data-enlighter-theme="" data-enlighter-highlight="" data-enlighter-linenumbers="true" data-enlighter-lineoffset="1" data-enlighter-title="@TITLE@" data-enlighter-group="@GROUPB2@">\nCOMMAND OUTPUT</pre>\n<!-- /wp:enlighter/codeblock -->\n\n\g<2>"""),
+
+	(r"Review project structure",
+	 r"""Review project structure\n\n<!-- wp:enlighter/codeblock {"linenumbers":"true","group":@GROUPC1@,"language":"shell","lineoffset":"1","title":"@TITLE@"} -->\n<pre class="EnlighterJSRAW" data-enlighter-language="shell" data-enlighter-theme="" data-enlighter-highlight="" data-enlighter-linenumbers="true" data-enlighter-lineoffset="1" data-enlighter-title="@TITLE@" data-enlighter-group="@GROUPC2@">\n$ tree --dirsfirst --filelimit 10</pre>\n<!-- /wp:enlighter/codeblock -->\n\n...\n"""),
+
+	# (r"""^(```)\n((.*\n)*?)^(```)""",
+	#  r"""<!-- wp:enlighter/codeblock {"linenumbers":"true","group":@GROUPD1@,"language":"shell","lineoffset":"1","title":"@TITLE@"} -->\n\n<pre class="EnlighterJSRAW" data-enlighter-language="shell" data-enlighter-theme="" data-enlighter-highlight="" data-enlighter-linenumbers="true" data-enlighter-lineoffset="1" data-enlighter-title="@TITLE@" data-enlighter-group="@GROUPD2@">\n\g<2></pre>\n<!-- /wp:enlighter/codeblock -->\n\n"""),
+
+
+	# (r"Figure X",
+	#  r"<strong>Figure X</strong>"),
+
+	# bold italics
+	(r"""\*\*\*(.*?)\*\*\*""",
+	 r"""<strong><em>\g<1></em></strong>"""),
+
+	# bold generic
+	(r"""\*\*(.*?)\*\*""",
+	 r"""<strong>\g<1></strong>"""),
+
+	# ##### must be under **double** stars #####
+	# italics
+	(r"""\*{1}(.*?)\*{1}""",
+	 r"""<em>\g<1></em>"""),
+
+	(r"""—""",
+	 r"""--"""),
+
+	# italics
+	#(r"""\_(.*?)\_""",
+	# r"""<em>\g<1></em>"""),
+
+	# italics (x, y)-coordinates
+	(r""" \(x, y\)-(.*) """,
+	 r""" <em>\(x, y\)</em>-\g<1> """),
+
+	(r"JUMP TO CODE DOWNLOAD",
+	 r"[jump_to_code_download]"),
+
+	(r"""CODE DOWNLOAD FORM""",
+	 r"""[code_download_form action="" formid=""]"""),
+
+	(r"""DOWNLOAD FORM""",
+	 r"""[code_download_form action="" formid=""]"""),
+
+	# preformatted filename .py
+	(r"`(.*?).py`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.py</code>"""),
+
+	# preformatted filename .zip
+	(r"`(.*?).zip`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.zip</code>"""),
+
+	# preformatted filename .json
+	(r"`(.*?).json`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.json</code>"""),
+
+	# preformatted filename .png
+	(r"`(.*?).png`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.png</code>"""),
+
+	# preformatted filename .h5
+	(r"`(.*?).h5`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.h5</code>"""),
+
+	# preformatted filename .txt
+	(r"`(.*?).txt`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.txt</code>"""),
+
+	# preformatted filename .avi
+	(r"`(.*?).avi`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.avi</code>"""),
+
+	# preformatted filename .hdf5
+	(r"`(.*?).hdf5`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.hdf5</code>"""),
+
+	# preformatted filename .gz
+	(r"`(.*?).gz`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.gz</code>"""),
+
+	# preformatted filename .pdf
+	(r"`(.*?).pdf`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.pdf</code>"""),
+
+	# preformatted filename .tflite
+	(r"`(.*?).tflite`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.tflite</code>"""),
+
+	# preformatted filename .pb
+	(r"`(.*?).pb`",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="shell">\g<1>.pb</code>"""),
+
+	# preformatted
+	(r"""`(.*?)`""",
+	 r"""<code class="EnlighterJSRAW" data-enlighter-language="python">\g<1></code>"""),
+
+	# fix preformatted inline zero
+	(r""">0<""",
+	 r"""> 0<"""),
+
+	# (r""".Downloads.""",
+	#  r"""<strong><em>"Downloads"</em></strong>"""),
+
+# 	(r"""\(x, y\)-(.*) """,
+# 	 r"""<em>(x, y)</em>-\g<1> """),
+
+# 	(r"""Line (\d*)""",
+# 	 r"""<strong>Line \g<1></strong>"""),
+
+# 	(r"""Lines (\d*)-(\d*)""",
+# 	 r"""<strong>Lines \g<1>-\g<2></strong>"""),
+
+# 	(r"""Lines (\d*) and (\d*)""",
+# 	 r"""<strong>Lines \g<1> and \g<2></strong>"""),
+
+# 	# MARKDOWN
+
+# 	# be careful with this one -- if there is a comment in a code block, it fails
+	(r"""^# (.*)""",
+	 r"""<h2>\g<1></h2>"""),
+
+	(r"""^## (.*)""",
+	 r"""<h3>\g<1></h3>"""),
+
+	(r"""^### (.*)""",
+	 r"""<h4>\g<1></h2>"""),
+
+	# (r"""_(.*)_""",
+	#  r"""<em>\g<1></em>"""),
+
+	# (r""" \*([a-zA-Z].*?)\* """,
+	#  r"""<em>\g<1></em>"""),
+
+	# (r"""\*\*(.*)\*\*""",
+	#  r"""<strong>\g<1></strong>"""),
+
+	# hyperlinks open in new tab
+	(r"""\[(.*?)\]\((.*?)\)""",
+	 r"""<a href="\g<2>" target="_blank" rel="noopener">\g<1></a>"""),
+
+
+	## NUMBERED LISTS   *****DON'T FOR GET TO CLOSE WITH <ol> MANUALLY*****
+	# numbered lists start with 1
+	(r"""^1\. (.*)""",
+	 r"""<ol>\n\t<li>\g<1></li>"""),
+
+	# numbered lists 2-onward
+	(r"""^[\d*]\. (.*)""",
+	 r"""\t<li>\g<1></li>"""),
+
+	## BULLETS   *****DON'T FOR GET TO OPEN and CLOSE WITH <ul> MANUALLY*****
+	(r"""^- (.*)""",
+	 r"""\t<li>\g<1></li>"""),
+
+
+	# with gutenberg and enlighter, all paragraphs get open and close <p> tags
+	(r"""^([A-Z\d\*\[].*)""",
+	 r"""<p>\g<1></p>"""),
+
+	# ...continuing, we then need to go back and remove the paragraph tags for codeblocks
+	(r"""^<p>(Codeblock.*)</p>""",
+	 r"""\g<1>""")
+
+
+]
--- regexbp.py
+++ regexbp.py
@@ -5,7 +5,7 @@ import re
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--format", type=str, required=True,
-	choices=["txt2wp", "md2wp", "md2latex", "temp"])
+	choices=["txt2wp", "md2wp-crayon", "md2wp-enlighter", "md2latex", "temp"])
 ap.add_argument("-i", "--input", type=str, required=True,
 	help="path to input text file")
 ap.add_argument("-o", "--output", type=str, required=True,
@@ -16,9 +16,17 @@ args = vars(ap.parse_args())
 
 # handle the type of config to import
 
-# Markdown -> wp html
-if args["format"] == "md2wp":
+# Markdown -> wp html ****CRAYON****
+if args["format"] == "md2wp-crayon":
 	import config_md2wp as config
+	if args["title"] == "":
+		print("[ERROR] you are formatting a blog post and you didn't "
+			"provide a title")
+		sys.exit(1)
+
+# Markdown -> wp html ****ENLIGHTER****
+elif args["format"] == "md2wp-enlighter":
+	import config_md2wp_enlighter as config
 	if args["title"] == "":
 		print("[ERROR] you are formatting a blog post and you didn't "
 			"provide a title")
@@ -84,18 +92,18 @@ with open(args["output"], "r") as o:
 	# new lines for the start of sentences (Wordpress spacing)
 	lines = re.sub(r"\.\n([A-Z])", ".\n\n\g<1>", lines)
 
-	if args["format"] == "latex":
+	if args["format"] == "md2latex":
 		lines = re.sub(r"[\s-]*COMMAND[\s-]*OUTPUT",
 			r"\n\n\\begin{minted}[xleftmargin=15pt,frame=lines,framesep=1mm]{console}\nCOMMAND + OUTPUT\n\\end{minted}\n",
 			lines)
 
-	elif args["format"] == "wp":
-		lines = re.sub(r"[\s-]*COMMAND[\s-]*OUTPUT",
-			r"""\n<pre class="start-line:1 lang:sh decode:true " title="@TITLE@">COMMAND OUTPUT</pre>\n""",
-			lines)
+	# elif args["format"] == "md2wp-crayon" or args["format"] == "md2wp-enlighter":
+	# 	lines = re.sub(r"[\s-]*COMMAND[\s-]*OUTPUT",
+	# 		r"""\n<pre class="start-line:1 lang:sh decode:true " title="@TITLE@">COMMAND OUTPUT</pre>\n""",
+	# 		lines)
 
+	# add the title to code blocks (a hack...see the config.py)
 	if args["title"] is not None:
-		# add the title to code blocks (a hack...see the config.py)
 		lines = re.sub(r"@TITLE@", args["title"], lines)
 
 
@@ -105,3 +113,45 @@ o.write(lines)
 
 # close the file
 o.close()
+
+# here begins the unique group number for enlighter blocks
+# each enligher block has a comment and the codeblock with a group number
+# regex above replaces  with @GROUPA@ and @GROUPAA@ <-- these correspond
+# in the loop below, we'll make the unique group number the line number
+# of the HTML file but we have to ensure that we maintain the number
+# to associate A with AA, B with BB, C with CC and so on
+
+if args["format"] == "md2wp-enlighter":
+	# open just the output file
+	with open(args["output"], "r") as o:
+		# read the whole file
+		lines = o.readlines()
+		lastI = None
+
+		print(lines[1])
+
+		for (i, line) in enumerate(lines):
+
+			if "@GROUPA1@" in line or "@GROUPB1@" in line or "@GROUPC1@" in line or "@GROUPD1@" in line:
+				lastI = i
+				line = re.sub(r"@GROUPA1@", str(i), line)
+				line = re.sub(r"@GROUPB1@", str(i), line)
+				line = re.sub(r"@GROUPC1@", str(i), line)
+				line = re.sub(r"@GROUPD1@", str(i), line)
+
+			elif "@GROUPA2@" in line or "@GROUPB2@" in line or "@GROUPC2@" in line or "@GROUPD2@" in line:
+				line = re.sub(r"@GROUPA2@", str(lastI), line)
+				line = re.sub(r"@GROUPB2@", str(lastI), line)
+				line = re.sub(r"@GROUPC2@", str(lastI), line)
+				line = re.sub(r"@GROUPD2@", str(lastI), line)
+				lastI = None
+
+			lines[i] = line
+
+
+	# open the file for writing and write the lines
+	with open(args["output"], "w") as o:
+		for item in lines:
+			o.write("%s\n" % item)
+
+
